d39526b3c47cc82c5f6d1f2bff1d323cc49ed635
Oksana_Dahaieva <oksanadagaeva@gmail.com>
2024-11-24 17:10:45 +0200

    added properties and set up quality gate for pull/merge requests

--- src/main/java/core/configuration/DriverManager.java
+++ src/main/java/core/configuration/DriverManager.java
@@ -5,20 +5,23 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class DriverManager {
 
-    private static WebDriver driver;
+  private static WebDriver driver;
 
-    public static WebDriver getDriver() {
-        if (driver == null) {
-            driver = new ChromeDriver();
-            driver.manage().window().maximize();
-        }
-        return driver;
+  public static WebDriver getDriver() {
+    if (driver == null) {
+      driver = new ChromeDriver();
+      driver
+          .manage()
+          .window()
+          .maximize();
     }
+    return driver;
+  }
 
-    public static void quitDriver() {
-        if (driver != null) {
-            driver.quit();
-            driver = null;
-        }
+  public static void quitDriver() {
+    if (driver != null) {
+      driver.quit();
+      driver = null;
     }
+  }
 }
--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -1,26 +1,28 @@
 package steps;
 
 import business.pages.LoginPage;
-import core.configuration.ConfigManager;
-import core.configuration.DriverManager;
+import core.utilities.WaitUtils;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
+import static core.configuration.DriverManager.getDriver;
+import static core.configuration.DriverManager.quitDriver;
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
 public class LoginSteps {
 
   private WebDriver driver;
+
   private LoginPage loginPage;
 
   @Before
   public void setUp() {
-    driver = DriverManager.getDriver();
+    driver = getDriver();
   }
 
   @Given("I open the Report Portal login page")
@@ -35,14 +37,11 @@ public class LoginSteps {
 
   @Then("I should see the dashboard")
   public void verifyLogin() {
-    String expectedUrl = "http://localhost:8080/ui/#automated_testing_global_mentoring_program/dashboard";
-    assertTrue(driver
-        .getCurrentUrl()
-        .contains(expectedUrl), "Dashboard is not visible. Login failed!");
+    assertEquals(loginPage.getSuccessMessageAfterLogin(), "Signed in successfully");
   }
 
   @After
   public void tearDown() {
-    DriverManager.quitDriver();
+    quitDriver();
   }
 }
