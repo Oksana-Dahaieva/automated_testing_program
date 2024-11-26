91263cc01eea0ded529916445a011f927ce5b2f4
Oksana_Dahaieva <oksanadagaeva@gmail.com>
2024-11-26 15:13:38 +0200

    added options

--- src/main/java/business/pages/LoginPage.java
+++ src/main/java/business/pages/LoginPage.java
@@ -6,8 +6,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-import java.time.Duration;
-
 public class LoginPage extends BasePage {
 
   public LoginPage(WebDriver driver) {
